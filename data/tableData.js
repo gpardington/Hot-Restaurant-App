1859bbccc87e182a440e6c7aa6d4210818440944
Grant Pardington <grant.pardington@gmail.com>
2020-09-02 01:22:39 -0600

    Finished with everything. Not working properly. Need to debug.

---
+++ data/tableData.js
@@ -0,0 +1,11 @@
+var tableArray = [
+    {
+      customerName: "Ahmed",
+      customerEmail: "ahmed@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+
+// Exporting the array makes it accessible to other files using require.
+module.exports = tableArray;
